711e8cb87fa2a73e672dae954c695c269fafa436
william1130 <william.t.1982@gmail.com>
2019-01-09 14:18:55 +0800

    Hello Spring Boot

---
+++ src/main/java/org/wm/someSB/Application.java
@@ -0,0 +1,13 @@
+package org.wm.someSB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+
+        System.out.println("Hi Some Spring Boot!!");
+    }
+}
